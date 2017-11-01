b7680374e629411e4bfc2c979b1ebf2e6870dd7b
kindsnake <kindsnake@163.com>
2017-11-02 02:09:59 +0800

    删除了每行代码末尾的^M
    删除了每行代码末尾的^M

--- index.php
+++ index.php
@@ -1,23 +1,21 @@
-<?php
-/**
- * 这是 Typecho 1.0 系统的一套皮肤，用于个人简历
- * 
- * @package Sunzx Theme 
- * @author Kindsnake
- * @version 0.4
- * @link http://www.sunzexiang.com
- * @date 07/28/2017
- */
-
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
- $this->need('header.php');
-?>
-
-<div class="contenter">
-    <div class="content">
-        <h1><?php $this->title() ?></h1>
-            <?php $this->content(); ?>
-	</div>
-</div> 
-
+<?php
+/**
+ * 这是 Typecho 1.0 系统的一套皮肤，用于个人简历
+ * 
+ * @package Sunzx Theme 
+ * @author Kindsnake
+ * @version 0.4
+ * @link http://www.sunzexiang.com
+ * @date 07/28/2017
+ */
+
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+    $this->need('header.php');
+?>
+<div class="contenter">
+    <div class="content">
+        <h1><?php $this->title() ?></h1>
+        <?php $this->content(); ?>
+	</div>
+</div> 
 <?php $this->need('footer.php'); ?>
